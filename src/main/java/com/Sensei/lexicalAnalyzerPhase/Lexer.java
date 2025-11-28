3ecc2b06f6930a01583d6715f3f0c4e9cf15758c
yahyaSensei <yahyahisham274@gmail.com>
2025-11-28 03:32:48 +0200

    add some functions to lexer

--- src/main/java/com/Sensei/lexicalAnalyzerPhase/Lexer.java
+++ src/main/java/com/Sensei/lexicalAnalyzerPhase/Lexer.java
@@ -1,4 +1,25 @@
 package com.Sensei.lexicalAnalyzerPhase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lexer {
+    private String source;
+    private List<Token> tokens=new ArrayList<Token>();
+    private int currentPosition=0;
+    private int startPosition=0;
+    private int line=1;
+    public Lexer(String source){
+        this.source=source;
+    }
+    private boolean isAtEnd(){
+        return source.length()>currentPosition;
+    }
+    public List<Token> tokenize(){
+        while(!isAtEnd()){
+        startPosition=currentPosition;
+        }
+        tokens.add(new Token(TokenType.EOF,"",null,line,startPosition,currentPosition));
+        return tokens;
+    }
 }
